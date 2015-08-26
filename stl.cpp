24128f47ac0952e7669d2d104e02a3f69d4d14c5
Shubhnkar <shubh800@gmail.com>
2015-08-27 01:59:16 +0530

    ceaser cipher

--- stl.cpp
+++ stl.cpp
@@ -17,21 +17,39 @@ int main()
         cin>>str[i];        
     }
     cin>>k;
+    int e=k;
     for(int i=0;i<n;++i)
     {
-        if(isalpha(str[i]))
+        if(isalpha(str[i]) && isupper(str[i]))
+        {
             str[i] = str[i]+k;
-        //if(!isalpha(str[i]))
-        //{
-          //  str[i] = str[i]-k;
-           // if(str[i]<97)
-            //{
-              //  temp = str[i]-97;
-          //  }
-            
-        //}
+            if(!isalpha(str[i])  || !isupper(str[i]))
+            {
+                str[i] = str[i]-k;
+                int t = 90-str[i];
+                k-=t;
+                while(k>26)
+                    k-=26;
+                str[i]=64+k;
+            }
+        }
+        else if(isalpha(str[i]) && islower(str[i]))
+        {
+            str[i] = str[i]+k;
+            if(!isalpha(str[i])  || !islower(str[i]))
+            {
+                str[i] = str[i]-k;
+                int t = 122-str[i];
+                k-=t;
+                while(k>26)
+                    k-=26;
+                str[i]=96+k;
+                
+            }
+        }
             
         cout<<(char)str[i];        
+        k=e;
     }
     
     
